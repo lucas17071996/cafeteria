477a197f72a9edb0c2a31bf556db594bd09d60c3
Lucas Soares <156090735+lucas17071996@users.noreply.github.com>
2024-04-09 15:34:10 -0300

    cafeteria aromas do café
    landing page feita utilizando as tecnologias html5, css3, javascript, e bootstrap

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const inputCheck = document.querySelector('#dark-mode')
+const element = document.querySelector('body')
+inputCheck.addEventListener('click',() => {
+    const mode = inputCheck.checked ? 'dark': 'light'
+    element.setAttribute("data-bs-theme", mode)
+})
