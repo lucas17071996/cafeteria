67922f59503a0b5ab4e8964a3fb60a521b5f4dd2
Lucas <lucassoares17071996@gmail.com>
2024-05-04 20:23:31 -0300

    implementação da função de atualizar dia, mês e ano em javascript, para o footer

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ inputCheck.addEventListener('click',() => {
     const mode = inputCheck.checked ? 'dark': 'light'
     element.setAttribute("data-bs-theme", mode)
 })
+function atualizarData() {
+    let data = new Date();
+    let dia = data.getDate();
+    let mes = data.getMonth() + 1;
+    let ano = data.getFullYear();
+
+    // Exibir a data no formato desejado (por exemplo, 'DD/MM/AAAA')
+    let dataFormatada = `${dia}/ ${mes}/ ${ano}`
+
+    // Atualizar o conteúdo HTML com a data atualizada
+    document.getElementById('dataAtual').innerHTML = `&copy; Desenvolvido por Lucas Soares ${dataFormatada}`;
+}
+window.onload = function() {
+    atualizarData();
+};
